bdcd2e65884e4477a08ec6414e1f100495bb37c7
Desper-18 <mubosha.97@gmail.com>
2021-12-12 16:11:15 +0300

    added Enter key recognition as the trigger for solve function

--- js/main.js
+++ js/main.js
@@ -8,12 +8,30 @@ function clearAll() {
 
 function backspace() {
     var exp = document.form.operationView.value;
-    document.form.operationView.value = exp.substring(0, exp.length-1);
+    document.form.operationView.value = exp.substring(0, exp.length - 1);
 }
 
 function solve() {
     var exp = document.form.operationView.value;
     if (exp) {
-        document.form.operationView.value = eval(exp);
+        document.form.operationView.value = parseFloat(eval(exp));
+        if (document.form.operationView.value.includes('.')) {
+            parseFloat(eval(exp)).toFixed(2);
+        }
+        parseFloat(eval(exp));
+    }
+    switch (exp) {
+        case 0.1 + 0.2:
+            return 0.3;
     }
 }
+
+const calcInput = document.getElementById('operationInput');
+
+calcInput.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+        e.preventDefault();
+        // console.log('Enter pressed');
+        document.getElementById('solve').click();
+    }
+});
